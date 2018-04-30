759e82fdee6dc1518b59432c1f5f5e051a550866
rousveiga <rosaveigaotero@outlook.com>
2018-04-30 13:37:45 +0200

    fixen cousas, non sei que fixen

--- src/interfaceExchange.sh
+++ src/interfaceExchange.sh
@@ -1,4 +1,4 @@
 #! /bin/bash
 
-cp p2p/RemoteClientInterface.java ../../ServerP2P/src
+cp p2p/RemoteClientInterface.java ../../ServerP2P/src/p2p
 cp ../../ServerP2P/src/p2p/RemoteServerInterface.java p2p
