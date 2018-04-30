5cc8f1a240c715e5a31f99e754396655a5168873
migueltb01 <migueltb01@gmail.com>
2018-04-30 13:11:33 +0200

    scripts

--- src/interfaceExchange.sh
+++ src/interfaceExchange.sh
@@ -1,4 +1,4 @@
 #! /bin/bash
 
 cp p2p/RemoteClientInterface.java ../../ServerP2P/src
-cp ../../ServerP2P/src/RemoteServerInterface.java p2p
+cp ../../ServerP2P/src/p2p/RemoteServerInterface.java p2p
